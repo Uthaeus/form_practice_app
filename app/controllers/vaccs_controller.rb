8cff6b2eea2d0df49425480dfb894feadff7eced
Uthaeus <romanlavery@gmail.com>
2018-05-01 16:06:07 -0600

    Began buildout for vaccs controller

--- app/controllers/vaccs_controller.rb
+++ app/controllers/vaccs_controller.rb
@@ -3,4 +3,30 @@ class VaccsController < ApplicationController
   def index
     @vaccs = Vacc.all
   end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+
+
 end
