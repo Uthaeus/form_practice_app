f38c4eb87865610614e1bd4a26610814593dc6bd
Uthaeus <romanlavery@gmail.com>
2018-05-01 23:52:14 -0600

    Created crud for vaccs and show, edit, index pages and forms

--- app/controllers/vaccs_controller.rb
+++ app/controllers/vaccs_controller.rb
@@ -1,15 +1,23 @@
 class VaccsController < ApplicationController
-
+  before_action :set_vacc, only: [:show, :edit, :update, :destroy]
   def index
     @vaccs = Vacc.all
   end
 
   def new
-
+    @vacc = Vacc.new
   end
 
   def create
+    @vacc = Vacc.new(vacc_params)
 
+    respond_to do |format|
+      if @vacc.save
+        format.html { redirect_to @vacc, notice: 'Successfully created' }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   def edit
@@ -17,7 +25,13 @@ class VaccsController < ApplicationController
   end
 
   def update
-
+    respond_to do |format|
+      if @vacc.update(vacc_params)
+        format.html { redirect_to @vacc, notice: 'Successfully updated' }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
   def show
@@ -25,8 +39,21 @@ class VaccsController < ApplicationController
   end
 
   def destroy
+    @vacc.destroy
 
+    respond_to do |format|
+      format.html { redirect_to vaccs_url, notice: 'Successfully removed' }
+    end
   end
 
+  private
+
+    def set_vacc
+      @vacc = Vacc.find(params[:id])
+    end
+
+    def vacc_params
+      params.require(:vacc).permit(:title)
+    end
 
 end
